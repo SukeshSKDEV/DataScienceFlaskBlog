043b2ce445fba3c22223c9586b469d0cb95acd80
Sukesh <sukeshskdev@gmail.com>
2023-02-26 19:37:35 +0530

    Creating Posts page and Designing Blog Page

--- main.py
+++ main.py
@@ -36,11 +36,21 @@ class Contacts(db.Model):
     date=db.Column(db.String(12), nullable=True)
     email = db.Column(db.String(20), nullable=False)
 
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content=db.Column(db.String(120), nullable=False)
+    date=db.Column(db.String(12), nullable=True)
+    slug = db.Column(db.String(21), nullable=False)
+    img_file = db.Column(db.String(21), nullable=False)
+    tagline = db.Column(db.String(120), nullable=False)
+
 
 
 @app.route('/')
 def home():
-    return render_template('index.html',params=params)
+    posts=Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html',params=params,posts=posts)
 
 @app.route('/about')
 def about():
@@ -68,9 +78,10 @@ def contact():
 
     return render_template('contact.html',params=params)
 
-@app.route('/post')
-def post():
-    return render_template('post.html',params=params)
+@app.route('/post/<string:post_slug>',methods=['GET'])
+def post_route(post_slug):
+    post=Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html',params=params,post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
