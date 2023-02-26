f10de540af8bc132376820ec65b83453a946d629
Sukesh <sukeshskdev@gmail.com>
2023-02-26 10:22:44 +0530

    Connecting Database

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/data_science'
+db = SQLAlchemy(app)
+
+'''sno name phone_num msg date email'''
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num=db.Column(db.String(12), nullable=False)
+    msg=db.Column(db.String(120), nullable=False)
+    date=db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+
 
 @app.route('/')
 def home():
@@ -11,8 +26,22 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact',methods=['GET','POST'])
 def contact():
+    '''sno name phone_num msg date email'''
+    if request.method=='POST':
+
+        # entry into the database
+        name=request.form['name']
+        phone=request.form['phone']
+        message=request.form['message']
+        email=request.form['email']
+
+        entry=Contacts(name=name,phone_num=phone,date=datetime.now(),msg=message,email=email)
+
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('contact.html')
 
 @app.route('/post')
